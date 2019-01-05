e6c600851c1b6f590b9c96a72e4f079c4ad6a94c
Artem Shevchenko <shevartsoft@gmail.com>
2019-01-05 22:20:08 +0200

    start work with first animation sample

---
+++ app/src/main/java/com/shevart/animations/comments/CommentActivity.kt
@@ -0,0 +1,13 @@
+package com.shevart.animations.comments
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.shevart.animations.R
+
+class CommentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+    }
+}
---
+++ app/src/main/java/com/shevart/animations/comments/Description.kt
@@ -0,0 +1,10 @@
+package com.shevart.animations.comments
+
+/**
+ *
+ *  In this package I will try to implement animations from Dribble shot. You can find gif with this animation
+ *  in com.shevart.animations.comments folder
+ *
+ *  https://dribbble.com/shots/4024098-Notifications-Section-Interaction-Fame-Lab
+ *
+ * */
